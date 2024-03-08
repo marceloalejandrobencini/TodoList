2115d7be42801206ca6ed915f2e21bc591413c02
marceloalejandrobencini <bencinimarcelo92@gmail.com>
2024-03-08 20:48:35 -0300

    proyecto ahora si finalizado

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,45 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 import { NewTodoForm } from "./NewTodoForm";
-
 import { TodoList } from './TodoList';
-
-
-
 
 const App = () => {
   const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    fetch('https://playground.4geeks.com/apis/fake/todos/user/NicolasJaramillo')
-      .then((response) => response.json())
-      .then((data) => {
-        const todos = data.map((todo) => ({
-          id: todo.id,
-          title: todo.label, 
-          completed: todo.done, 
-        }));
-        setTodos(todos);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-
-  function syncTodosWithServer(todos) {
-    const serverTodos = todos.map((todo) => ({
-      label: todo.title, 
-      done: todo.completed, 
-    }));
-
-    fetch('https://playground.4geeks.com/apis/fake/todos/user/NicolasJaramillo', {
-      method: "PUT",
-      body: JSON.stringify(serverTodos),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .catch((error) => console.log(error));
-  }
 
   function addTodo(title) {
     setTodos((currentTodos) => {
@@ -47,7 +12,6 @@ const App = () => {
         ...currentTodos,
         { id: crypto.randomUUID(), title, completed: false },
       ];
-      syncTodosWithServer(newTodos);
       return newTodos;
     });
   }
@@ -60,7 +24,6 @@ const App = () => {
         }
         return todo;
       });
-      syncTodosWithServer(newTodos);
       return newTodos;
     });
   }
@@ -68,15 +31,14 @@ const App = () => {
   function deleteTodo(id) {
     setTodos((currentTodos) => {
       const newTodos = currentTodos.filter((todo) => todo.id !== id);
-    syncTodosWithServer(newTodos);
-    return newTodos;
+      return newTodos;
     });
   }
 
   function deleteAllTodos() {
     setTodos([]);
-    syncTodosWithServer([]);
   }
+
   return (
     <>
       <NewTodoForm addTodo={addTodo} />
